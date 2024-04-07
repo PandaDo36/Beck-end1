5ffa81e54e5784f6374cd7df31a13a5af68e8c05
PandaDo36 <lucaspanda95@hotmail.com>
2024-04-07 09:01:03 -0300

    criando configs, controllers, middlewares e services

--- routes/clientes.js
+++ routes/clientes.js
@@ -1,24 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+const clienteController = require('../controllers/clienteController');
+const nomeMiddleware = require('../middlewares/nomeMiddleware');
+const sobrenomeMiddleware = require('../middlewares/sobrenomeMiddleware');
+const idadeMiddleware = require('../middlewares/idadeMiddleware');
+
+
+ /* POST clientes*/
+ router.post('/', nomeMiddleware.validateName,
+ sobrenomeMiddleware.validateFamilyName,
+ idadeMiddleware.validateAge,
+ clienteController.save,
+ );
+
 /* GET roda da cliente listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource GET');
-});
+router.get('/', clienteController.findAll);
 
 /* PUT roda da cliente listing. */
-router.put('/', function (req, res, next) {
-  res.send('respond with a resource PUT');
-});
+router.put('/', clienteController.update);
 
 /* POST roda da cliente listing. */
-router.post('/', function (req, res, next) {
-  res.send('respond with a resource POST');
-});
+router.post('/', clienteController.save);
 
 /* DELETE roda da cliente listing. */
-router.delete('/', function (req, res, next) {
-  res.send('respond with a resource DELETE');
-});
+router.delete('/:id', clienteController.remove);
 
 module.exports = router;
